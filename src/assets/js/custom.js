3386464842f9bd16a688e8490762227344bf9edd
Ellis Sutehall <ellissutehall@gmail.com>
2019-03-15 19:59:04 +0000

    Initial commit

---
+++ src/assets/js/custom.js
@@ -0,0 +1,4 @@
+window.sr = ScrollReveal({ duration: 2000 }, { distance: '200px' } );
+sr.reveal('.rev-left', { origin: 'left' });
+sr.reveal('.rev-center', { origin: 'bottom' });
+sr.reveal('.rev-right', { origin: 'right' });
